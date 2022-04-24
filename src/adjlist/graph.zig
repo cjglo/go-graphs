c6c80f112b3e0e8a1a792142e85f85531190d506
cjglo <chris.j.gallo3@gmail.com>
2022-04-24 14:57:39 -0400

    Working on basic structure of zig graph struct

--- src/adjlist/graph.zig
+++ src/adjlist/graph.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
 pub fn Graph(comptime T: type) type {
-    
-    return struct {
+    return struct { // Graph itself
         const Self = @This();
-    }
+
+        // Vertex and Edge Structs //TODO: should incidence be own struct? Edges will need pointers to there specific spot in specific incidence (double ptr)
+        pub const Vertex = struct { name: T, index: u64, incidence: []?*Edge };
+
+        pub const Edge = struct {};
+
+        pub fn readFromFile() void {
+            
+        }
+    };
 }
--- src/adjlist/graph_test.zig
+++ src/adjlist/graph_test.zig
@@ -1,33 +1,34 @@
 const std = @import("std");
+const graph = @import("graph.zig");
 
-const TestError = error {
-    Unimplemented,
-};
-
+const TestError = error{ Unimplemented, GraphInitFailure };
 
 // == Tests ==
 
-test "Expect ReadFromFile to initialize Graph successfully" {
+test "Expect blank Graph to be initialized succesfully" {
+    const g = graph.Graph([]const u8);
 
+    if(@TypeOf(g) != type) {
+        return TestError.GraphInitFailure;
+    }
+}
+
+test "Expect ReadFromFile to initialize Graph successfully" {
     return TestError.Unimplemented;
 }
 
 test "Expect FindShortestPath to return shortest path from \"a\" to \"g\" to be 15" {
-
     return TestError.Unimplemented;
 }
 
 test "Expect FindShortestPath to return shortest path from \"f\" to \"d\" to be 17" {
-
     return TestError.Unimplemented;
 }
 
 test "Expect DoesNameExistInGraph to return true for \"a\"" {
-
     return TestError.Unimplemented;
 }
 
 test "Expect DoesNameExistInGraph to return false for \"z\"" {
-    
     return TestError.Unimplemented;
 }
