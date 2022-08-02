d2595016c6c5d352934e7afca7e55dc93bb0b219
cjglo <chris.j.gallo3@gmail.com>
2022-08-01 21:58:17 -0400

    setting up adj matrix

---
+++ src/matrix/graphs.h
@@ -0,0 +1,12 @@
+#ifndef __GRAPHS_H_
+#define __GRAPHS_H_
+
+namespace M {
+    class Matrix {
+        public:
+        
+    };
+
+}
+
+#endif
---
+++ src/matrix/tests.h
@@ -0,0 +1,7 @@
+#ifndef __TESTS_H_
+#define __TESTS_H_
+
+#include "graphs.h"
+#include <iostream>
+
+#endif
